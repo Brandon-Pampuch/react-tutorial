efab431e414189685059e1d769db02ac8b0abb86
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-11-10 13:59:30 -0600

    fake back end complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,16 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+const authConfig = require('./authconfig.json')
 
 // define the Express app
 const app = express();
 
 // the database
-const questions = [];
+const questions = [
+  { id: 1, title: "yolo", description: "you only live once", answers: ["yes", "no"] },
+  { id: 2, title: "fubar", description: "fucked up beyond belief", answers: ["no", "yes"] }
+];
 
 // enhance your app security with Helmet
 app.use(helmet());
@@ -25,42 +29,46 @@ app.use(cors());
 // log HTTP requests
 app.use(morgan('combined'));
 
+
+
 // retrieve all questions
-app.get('/', (req, res) => {
+app.get('/', checkJwt, (req, res) => {
   const qs = questions.map(q => ({
     id: q.id,
     title: q.title,
     description: q.description,
-    answers: q.answers.length,
+    answers: q.answers.length
   }));
   res.send(qs);
 });
+// function for verifying jwt
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: authConfig.audience, // name of api //identifier in settings
+  issuer: `https://${authConfig.domain}/`, //local host
+  algorithms: ['RS256']
+});
 
 // get a specific question
-app.get('/:id', (req, res) => {
+app.get('/:id', checkJwt, (req, res) => {
   const question = questions.filter(q => (q.id === parseInt(req.params.id)));
   if (question.length > 1) return res.status(500).send();
   if (question.length === 0) return res.status(404).send();
   res.send(question[0]);
 });
 
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://bk-tmp.auth0.com/.well-known/jwks.json`
-  }),
 
-  // Validate the audience and the issuer.
-  audience: 'PVafIu9Q5QN65DiPByAFvCCJryY7n432',
-  issuer: `https://bk-tmp.auth0.com/`,
-  algorithms: ['RS256']
-});
 
 // insert a new question
 app.post('/', checkJwt, (req, res) => {
-  const {title, description} = req.body;
+  const { title, description } = req.body;
   const newQuestion = {
     id: questions.length + 1,
     title,
@@ -74,7 +82,7 @@ app.post('/', checkJwt, (req, res) => {
 
 // insert a new answer to a question
 app.post('/answer/:id', checkJwt, (req, res) => {
-  const {answer} = req.body;
+  const { answer } = req.body;
 
   const question = questions.filter(q => (q.id === parseInt(req.params.id)));
   if (question.length > 1) return res.status(500).send();
@@ -92,3 +100,5 @@ app.post('/answer/:id', checkJwt, (req, res) => {
 app.listen(8081, () => {
   console.log('listening on port 8081');
 });
+
+
